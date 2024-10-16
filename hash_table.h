de8ea66b9eeefe1838f127f6ad36dfcd5847999c
Aryan <161190212+aryan-raj-05@users.noreply.github.com>
2024-10-16 15:58:38 +0000

    implemented search, insert and delete function for the hash table

--- hash_table.h
+++ hash_table.h
@@ -11,3 +11,9 @@ typedef struct ht_hash_table {
 
 #define HT_PRIME_1 151
 #define HT_PRIME_2 157
+
+void ht_insert(ht_hash_table *ht, const *key, const char *value);
+
+char *ht_search(ht_hash_table *ht, const char *key);
+
+void ht_delete(ht_hash_table *h, const char *key);
